353e8ea1cb04624216c8f56bf10a5b15438b1aef
Mallikarjun-reddy11 <mallikarjunpandre55@gmail.com>
2026-07-28 19:47:42 +0530

    files added

--- Comination/Main2.java
+++ Comination/Main2.java
@@ -1,21 +1,18 @@
 package Comination;
 
 public class Main2 {
-public static void main(String[] args) {
-	
-	
-		CustomerBot c=new CustomerBot(101,"Customer AI","1.0","openAI","API123");
-		
+	public static void main(String[] args) {
+
+		CustomerBot c = new CustomerBot(101, "Customer AI", "1.0", "openAI", "API123");
 
 		c.display();
 		c.display1();
-		CodeBot c1=new CodeBot(102,"Code Related AI","1.20","OpenAI","API432");
+		CodeBot c1 = new CodeBot(102, "Code Related AI", "1.20", "OpenAI", "API432");
 		c1.display();
 		c1.display1();
-		HealthcareBot h=new HealthcareBot(103,"Healthcare AI","1.30","OpenAI","API434");
+		HealthcareBot h = new HealthcareBot(103, "Healthcare AI", "1.30", "OpenAI", "API434");
 		h.display();
 		h.display1();
-		
-	
-}
+
+	}
 }
