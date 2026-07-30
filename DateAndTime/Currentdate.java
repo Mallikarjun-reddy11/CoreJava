2c8394d5a8beaa003c0d2e28d4c52d1dbd6c91fa
Mallikarjun-reddy11 <mallikarjunpandre55@gmail.com>
2026-07-30 20:54:11 +0530

    files added

---
+++ DateAndTime/Currentdate.java
@@ -0,0 +1,10 @@
+package DateAndTime;
+import java.util.Date;
+public class Currentdate {
+public static void main(String[] args) {
+	Date d1=new Date ();
+	System.out.println(d1);
+	
+    System.out.println("Class Name: " + d1.getClass().getName());
+}
+}
