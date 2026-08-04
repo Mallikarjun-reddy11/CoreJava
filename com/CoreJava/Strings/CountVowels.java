834456f9a8ee7741a38a3a5dbbc4a29063f3decf
Mallikarjun-reddy11 <mallikarjunpandre55@gmail.com>
2026-08-04 10:51:01 +0530

    files added

--- com/CoreJava/Strings/CountVowels.java
+++ com/CoreJava/Strings/CountVowels.java
@@ -1,21 +1,22 @@
 package com.CoreJava.Strings;
 
 public class CountVowels {
-String s="Mallikarjun";
-public void countV() {
-	int count=0;
-	s=s.toLowerCase();
-	for(int i=0;i<s.length();i++) {
-		char ch=s.charAt(i);
-		if(ch=='a'|| ch=='e' ||ch=='i' || ch=='o'|| ch=='U') {
-			count++;
-		}
-	}
-	System.out.println(count);
-}
-public static void main(String []args) {
-	CountVowels c=new CountVowels();
-	c.countV();
-}
-}
+	String s = "Mallikarjun";
 
+	public void countV() {
+		int count = 0;
+		s = s.toLowerCase();
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'U') {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+	public static void main(String[] args) {
+		CountVowels c = new CountVowels();
+		c.countV();
+	}
+}
---
+++ com/CoreJava/Strings/Reverse.java
@@ -0,0 +1,26 @@
+package com.CoreJava.Strings;
+
+public class Reverse {
+
+	String s = "mom";
+
+	public void reverseS() {
+		String rev = "";
+		for (int i = s.length() - 1; i >= 0; i--) {
+			char ch = s.charAt(i);
+			rev = rev + ch;
+		}
+		if (s.equals(rev)) {
+			System.out.println("Palindrome");
+		} else {
+			System.out.println("Not a palindrome");
+
+		}
+		System.out.println(rev);
+	}
+
+	public static void main(String[] args) {
+		Reverse r = new Reverse();
+		r.reverseS();
+	}
+}
