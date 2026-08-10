485423470cb37178219397a8ad727531ddc4fc06
Mallikarjun-reddy11 <mallikarjunpandre55@gmail.com>
2026-08-10 23:10:22 +0530

    files added

---
+++ com/CoreJava/Strings/WordsLength.java
@@ -0,0 +1,9 @@
+package com.CoreJava.Strings;
+
+public class WordsLength {
+public static void main(String[] args) {
+	String s="Iam a Java developer";
+	String[]  words=s.split(" ");
+	System.out.println(words.length);
+}
+}
