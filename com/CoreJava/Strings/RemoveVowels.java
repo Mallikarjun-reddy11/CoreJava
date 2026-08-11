fde778ef48dce723a923e3b87d320257478a2d3e
Mallikarjun-reddy11 <mallikarjunpandre55@gmail.com>
2026-08-11 12:32:39 +0530

    files added

---
+++ com/CoreJava/Strings/RemoveVowels.java
@@ -0,0 +1,18 @@
+package com.CoreJava.Strings;
+
+public class RemoveVowels {
+
+	public static void main(String[] args) {
+	 String name="Mallikarjun";
+	 String res="";
+	 name=name.toLowerCase();
+	 for(int i=0;i<name.length();i++) {
+		 char ch=name.charAt(i);
+		 if(ch!='a'&& ch!='e'&&ch!='i'&&ch!='o'&&ch!='u') {
+			 res+=ch;
+		 }
+	 }
+System.out.println(res);
+	}
+
+}
