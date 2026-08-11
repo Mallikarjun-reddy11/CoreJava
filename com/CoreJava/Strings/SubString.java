a213569bb4d0f5a7bb3a162f74762a2345fa7f99
Mallikarjun-reddy11 <mallikarjunpandre55@gmail.com>
2026-08-11 11:31:54 +0530

    files added

---
+++ com/CoreJava/Strings/SubString.java
@@ -0,0 +1,10 @@
+package com.CoreJava.Strings;
+
+public class SubString {
+	public static void main(String[] args) {
+	
+String name="Iam a java developer";
+System.out.println(name.replace("Iam a java", "java"));
+
+}
+}
