870f61bbf881708d85ecfbf98f28707817ba34ad
Mallikarjun-reddy11 <mallikarjunpandre55@gmail.com>
2026-08-12 21:16:42 +0530

    files added

--- com/CoreJava/Exceptionandlin/AgeVerification.java
+++ com/CoreJava/Exceptionandlin/AgeVerification.java
@@ -2,10 +2,15 @@ package com.CoreJava.Exceptionandlin;
 
 public class AgeVerification {
 	public static void main(String[] args) {
-		int age;
+		int age = 0;
 try {
 	if(age<18) {
-		
+		System.out.println("Eligible");
+	}else {
+		System.out.println("not eligible");
+	}
+}catch(Exception e) {
+	System.out.println(e.getMessage());
+}
 	}
 }
-}}
--- com/CoreJava/Strings/RemoveVowels.java
+++ com/CoreJava/Strings/RemoveVowels.java
@@ -3,16 +3,16 @@ package com.CoreJava.Strings;
 public class RemoveVowels {
 
 	public static void main(String[] args) {
-	 String name="Mallikarjun";
-	 String res="";
-	 name=name.toLowerCase();
-	 for(int i=0;i<name.length();i++) {
-		 char ch=name.charAt(i);
-		 if(ch!='a'&& ch!='e'&&ch!='i'&&ch!='o'&&ch!='u') {
-			 res+=ch;
-		 }
-	 }
-System.out.println(res);
+		String name = "Mallikarjun";
+		String res = "";
+		name = name.toLowerCase();
+		for (int i = 0; i < name.length(); i++) {
+			char ch = name.charAt(i);
+			if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
+				res += ch;
+			}
+		}
+		System.out.println(res);
 	}
 
 }
